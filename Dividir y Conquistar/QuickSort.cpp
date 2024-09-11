7b8b36b13a04547dd936577bf920c3fef2b782f0
Connie Estay <116611269+connie-sykes@users.noreply.github.com>
2024-09-10 22:40:26 -0300

    Update QuickSort.cpp

--- Dividir y Conquistar/QuickSort.cpp
+++ Dividir y Conquistar/QuickSort.cpp
@@ -1,1 +1,75 @@
+// C++ Program to demonstrate how to implement the quick
+// sort algorithm
+#include <bits/stdc++.h>
+using namespace std;
 
+int partition(vector<int> &vec, int low, int high) {
+
+    // Selecting last element as the pivot
+    int pivot = vec[high];
+
+    // Index of elemment just before the last element
+    // It is used for swapping
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++) {
+
+        // If current element is smaller than or
+        // equal to pivot
+        if (vec[j] <= pivot) {
+            i++;
+            swap(vec[i], vec[j]);
+        }
+    }
+
+    // Put pivot to its position
+    swap(vec[i + 1], vec[high]);
+
+    // Return the point of partition
+    return (i + 1);
+}
+
+void quickSort(vector<int> &vec, int low, int high) {
+
+    // Base case: This part will be executed till the starting
+    // index low is lesser than the ending index high
+    if (low < high) {
+
+        // pi is Partitioning Index, arr[p] is now at
+        // right place
+        int pi = partition(vec, low, high);
+
+        // Separately sort elements before and after the
+        // Partition Index pi
+        quickSort(vec, low, pi - 1);
+        quickSort(vec, pi + 1, high);
+    }
+}
+// Funcion que imprime un vector
+void printVector(vector<int>& arr)
+{
+    for (int i = 0; i < arr.size(); i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    vector<int> arr = {} ;
+    
+    //Paso datos del dataset a un vector para ordenar
+    int num;
+    while(cin >> num){
+        arr.push_back(num);
+    }
+    int n = arr.size();    
+    auto start = chrono::high_resolution_clock::now();
+
+    quickSort(arr, 0, n - 1);
+
+    auto end = chrono::high_resolution_clock::now();
+    chrono::duration<double> duration = end - start;
+
+    cout << "Tiempo en segundos: "<< duration.count() << endl;
+
+    return 0;
+}
